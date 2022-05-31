ef753398190afc0591298541845989b11905977f
haowencs <haowen_cs@hotmail.com>
2022-05-31 21:20:19 +0800

    avoid tensorflow exp

--- mixed_test_func/__init__.py
+++ mixed_test_func/__init__.py
@@ -1,3 +1,3 @@
 from .synthetic import *
 from .xgboost_hp import *
-from .AdvAttack.bbattack_wrapped import AdversarialAttack
+# from .AdvAttack.bbattack_wrapped import AdversarialAttack // avoid tensorflow
